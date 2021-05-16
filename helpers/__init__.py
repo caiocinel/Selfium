96db40756a5fec281a41cbe134f084205a955aa2
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-05-16 15:07:45 -0300

    Created helpers folder

---
+++ helpers/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+Selfium Helper Files
+~~~~~~~~~~~~~~~~~~~
+All Helper Files used in Selfium project;
+:copyright: (c) 2021 - Caillou and ZeusHay;
+:license: MIT, see LICENSE for more details.
+"""
+
+from .getMemberAvatarURL import *
