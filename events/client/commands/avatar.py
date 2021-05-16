a91deec253811939023f34c8ed345af97499950b
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-05-16 19:01:56 -0300

    Added avatar grabber command
    Usage:
    
    <prefix>avatar @metion or ID
    
    If no arguments, the itself avatar will be displayed

---
+++ events/client/commands/avatar.py
@@ -0,0 +1,19 @@
+import discord
+from vars.client import client
+from helpers import delete
+from cli import notify
+
+@client.command(aliases=['Avatar'])
+async def avatar(ctx, Member: discord.Member=None):
+    try:
+        if(Member):
+            target = Member
+        else:
+            target = ctx.author
+        await delete.byContext(ctx)
+        embed = discord.Embed(colour=discord.Colour.green())
+        embed.set_author(name=f"🖼️ Here's {target.display_name} profile picture", url=f"{target.avatar_url}")
+        embed.set_image(url=target.avatar_url)
+        await ctx.send(embed=embed)
+    except:
+        await notify.error(ctx, f'Something went wrong, try again!', None, 5)
