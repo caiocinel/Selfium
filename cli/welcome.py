e4b82ba949a9689990c85b5f62ef15bf38b88596
ZeusHay <45320825+ZeusHay@users.noreply.github.com>
2021-05-16 02:35:50 -0300

    [ + ] - Changed logo design, added on_message event, added login info.

--- cli/welcome.py
+++ cli/welcome.py
@@ -1,4 +1,14 @@
+import platform, discord
+from auth.client import client
 from colorama import Fore
+from vars.client import client as self
+from datetime import datetime
+Time = datetime.now()
 
-def welcome():
-    print(f"{Fore.CYAN}> Successful login .")
+async def welcome_message():
+    print(f"{Fore.CYAN}[+] - Successful login")
+    print(f"""    > {Fore.GREEN}Connected As: {Fore.BLUE}{client.user.display_name}{Fore.CYAN}.
+    > {Fore.GREEN}Connected At: {Fore.BLUE}{Time.strftime("%H:%M:%S")} | {Time.strftime("%Y-%m-%d")}{Fore.CYAN}.
+    > {Fore.GREEN}PyVersion: {Fore.BLUE}{platform.python_version()}{Fore.CYAN}.
+    > {Fore.GREEN}DpVersion: {Fore.BLUE}{discord.__version__}{Fore.CYAN}.
+    """)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cli
 import auth
 
-
 if __name__ == '__main__':
     cli.logo()
     auth.run()
