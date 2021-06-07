2c3890a782ea33eab939b04adc25af55c18839ef
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-07 20:02:05 -0300

    Converted ban to d.py-self
    Now you can ban multiple members at once!

--- events/client/commands/ban.py
+++ events/client/commands/ban.py
@@ -1,17 +1,27 @@
 import discord
+import asyncio
 from vars.client import client
 from helpers import notify
 
 @client.command()
-async def ban(ctx, Member: discord.Member = None):
+async def ban(ctx):
     try:
-        if not Member or Member == ctx.author:
-            await notify.error(ctx, "No user found", None, 5)
+        if not ctx.message.mentions:
+            await notify.error(ctx, "No user found", 5)
             return
-        if ctx.message.author.guild_permissions.ban_members:
-            await ctx.guild.ban(Member)
-            await notify.success(ctx, f'You have successfully banned the user {Member.display_name}!', None, 8)
         else:
-            await notify.error(ctx, 'You are not allowed to ban here :( ', None, 5)
-    except:
-        notify.exception()
+            target = ctx.message.mentions
+
+        if ctx.message.author.guild_permissions.ban_members:
+
+            for t in range(len(target)):
+                await asyncio.sleep(0.3)
+                await ctx.guild.ban(target[t])
+                await notify.success(ctx, f'You have successfully banned the user {target[t].display_name}!', 8)
+
+        else:
+            await notify.error(ctx, 'You are not allowed to ban here :( ', 5)
+
+    except Exception as e:
+        await notify.exception(ctx, e)
+
