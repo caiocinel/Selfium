073f7921845d3abbce6de3baffa1708618158fd3
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-08 23:27:15 -0300

    Preview sendToAll

--- events/client/commands/debug.py
+++ events/client/commands/debug.py
@@ -1,9 +1,18 @@
 import discord
+import asyncio
 from vars.client import client
-from helpers import delete, notify
+from helpers import delete, notify, getUser
 
 @client.command()
 async def debug(ctx):
+    
+    a = ctx.guild._members
+    for member in a:
+        user = await getUser.byID(member)
+        if user.id != ctx.author.id:
+            await user.create_dm()
+            await user.send('Boa noite')
+            print(user.name)
+            await asyncio.sleep(2)
     return
-
-    print(b)
+    print(a)
