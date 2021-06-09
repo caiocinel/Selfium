269c6a60a2ebc8d14fabf54a8cd7ae624a9f1d94
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-08 22:09:50 -0300

    Fixed Avatar (Again)
    ¯\_(ツ)_/¯

--- events/client/commands/avatar.py
+++ events/client/commands/avatar.py
@@ -1,6 +1,6 @@
 import discord
 from vars.client import client
-from helpers import delete, notify
+from helpers import delete, notify, getUser
 
 @client.command(aliases=['profilepicture'])
 async def avatar(ctx):
@@ -11,9 +11,13 @@ async def avatar(ctx):
         target = ctx.message.author
 
     await delete.byContext(ctx)
-    for t in range(len(target)):
-        embed = discord.Embed(colour=discord.Colour.green())
-        embed.set_author(name=f"🖼️ Here's {target[t].display_name} profile picture", url=f"{target[t].avatar_url.BASE + target[t].avatar_url._url}")
-        embed.set_image(url=target[t].avatar_url.BASE + target[t].avatar_url._url)
-        await ctx.send(embed=embed)
+    try:
+        for t in range(len(target)):
+            memberFetch = await getUser.byMember(target[t])
+            embed = discord.Embed(colour=discord.Colour.green())
+            embed.set_author(name=f"🖼️ Here's {memberFetch.display_name} profile picture", url=f"{memberFetch.avatar_url.BASE + memberFetch.avatar_url._url}")
+            embed.set_image(url=memberFetch.avatar_url.BASE + memberFetch.avatar_url._url)
+            await ctx.send(embed=embed)
+    except:
+        notify.error(ctx, 'Member not found')
 
