884795833aecac4d8e63ef7e3dd09b7e7bd51e40
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-15 09:08:51 -0300

    rename logs to data in gift cfg

--- app/filesystem/gift.py
+++ app/filesystem/gift.py
@@ -1,11 +1,11 @@
 import json
 
 def load():
-    with open("logs/nitro.json") as giftFile:
+    with open("data/nitro.json") as giftFile:
         return json.load(giftFile)
     
 
 def save(data):
-    with open("logs/nitro.json", "w") as giftFile:
+    with open("data/nitro.json", "w") as giftFile:
         giftFile.write(json.dumps(data))
     giftFile.close()
