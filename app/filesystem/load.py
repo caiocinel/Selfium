31eb83e19fabfb5a2c6c6de63dd55cbe9e5e183f
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-17 14:32:18 -0300

    Added reloadConfig funcion

--- app/filesystem/load.py
+++ app/filesystem/load.py
@@ -3,3 +3,9 @@ import json
 with open("data/config.json") as loadFile:
     cfg = json.load(loadFile)
 loadFile.close()
+
+def reloadCfg():   
+    global cfg
+    with open("data/config.json") as loadFile:
+        cfg = json.load(loadFile)
+    loadFile.close()
