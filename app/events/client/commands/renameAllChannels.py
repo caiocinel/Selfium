502329cd85277e8270e0ab7dca97e11400baba9c
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-18 13:26:07 -0300

    Added Rename All Channels
    Can be used with Text | Voice | Category
    
    If no type is passed, all channels will be renamed

---
+++ app/events/client/commands/renameAllChannels.py
@@ -0,0 +1,33 @@
+import asyncio
+from discord.ext import commands
+from app.vars.client import client
+from app.helpers import notify, delete, params
+
+
+@client.command()
+@commands.guild_only()
+@commands.has_permissions(manage_channels=True)
+async def renameAllChannels(ctx, *, args = ''):
+    await delete.byContext(ctx)
+    args = params.split(args)
+    if len(args) > 1:
+        channelType = str.lower(args[1])
+        if(channelType == 'text'):
+            type = ctx.guild.text_channels
+        elif(channelType == 'voice'):
+            type = ctx.guild.voice_channels
+        elif(channelType == 'category'):
+            type = ctx.guild.categories
+        else:
+            notify.error(ctx, f'The type of channel you provide ({args[1]}) is not supported')
+    else:
+        type = ctx.guild.channels
+
+    for channel in type:
+        try:
+            await channel.edit(name=args[0])
+            await asyncio.sleep(0.33)
+        except:
+            pass         
+    else:
+        await notify.success(ctx, f'Successful renamed all channels to {args[0]}')    
