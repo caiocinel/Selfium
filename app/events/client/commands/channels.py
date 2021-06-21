35754ea88cdc888baedc22db9479fe1c1d294559
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-06-21 15:19:27 -0300

    Added Individual Channel Type List
    Added:
    textChannels
    voiceChannels
    Categories

--- app/events/client/commands/channels.py
+++ app/events/client/commands/channels.py
@@ -24,3 +24,46 @@ async def channels(ctx):
     except Exception as e:
         print(e)
         await notify.error(ctx, "Something goes wrong, check console for logs")
+
+
+@commands.guild_only()
+@client.command(aliases=["voice_channels"])
+async def voiceChannels(ctx):
+    try:
+        embed = discord.Embed(title="Voice Channels", colour=discord.Color.purple())
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        for channel in ctx.guild.voice_channels:
+            embed.add_field(name="\u2800", value=f"```{channel.name}```")
+        embed.set_footer(text="Selfium (◔‿◔)")  
+        await ctx.send(embed=embed)
+    except Exception as e:
+        print(e)
+        await notify.error(ctx, "Something goes wrong, check console for logs")
+
+@commands.guild_only()
+@client.command(aliases=["text_channels"])
+async def textChannels(ctx):
+    try:
+        embed = discord.Embed(title="Text Channels", colour=discord.Color.purple())
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        for channel in ctx.guild.text_channels:
+            embed.add_field(name="\u2800", value=f"```{channel.name}```")
+        embed.set_footer(text="Selfium (◔‿◔)")  
+        await ctx.send(embed=embed)
+    except Exception as e:
+        print(e)
+        await notify.error(ctx, "Something goes wrong, check console for logs")
+
+@commands.guild_only()
+@client.command()
+async def Categories(ctx):
+    try:
+        embed = discord.Embed(title="Categories", colour=discord.Color.purple())
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        for channel in ctx.guild.categories:
+            embed.add_field(name="\u2800", value=f"```{channel.name}```")
+        embed.set_footer(text="Selfium (◔‿◔)")  
+        await ctx.send(embed=embed)
+    except Exception as e:
+        print(e)
+        await notify.error(ctx, "Something goes wrong, check console for logs")
