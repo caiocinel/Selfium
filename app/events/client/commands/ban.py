eafc87387fa2cbd4a279c1bb3333efce0da2f303
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-11 23:35:17 -0300

    Changed ban to use Discord.Member class
    Same as Avatar

--- app/events/client/commands/ban.py
+++ app/events/client/commands/ban.py
@@ -7,17 +7,13 @@ from app.helpers import notify
 @client.command()
 @commands.guild_only()
 @commands.has_permissions(ban_members=True)
-async def ban(ctx):
+async def ban(ctx, Member: commands.Greedy[discord.Member]=None):
+    if not Member: await notify.error(ctx,'No users were informed'); return
     try:
-        if not ctx.message.mentions:
-            await notify.error(ctx, "No user found", 5)
-            return
-        else:
-            target = ctx.message.mentions
-        for t in range(len(target)):
+        for t in range(len(Member)):
             await asyncio.sleep(0.3)
-            await ctx.guild.ban(target[t])
-            await notify.success(ctx, f'You have successfully banned the user {target[t].display_name}!', 8)
+            await ctx.guild.ban(Member[t])
+            await notify.success(ctx, f'You have successfully banned the user {Member[t].display_name}!', 8)
     except Exception as e:
         await notify.exception(ctx, e)
 
