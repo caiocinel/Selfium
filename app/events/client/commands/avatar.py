347b85513c3a26b347b73894dff382fc82d9b1b5
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-11 23:30:16 -0300

    Adapted avatar command using discord.Member class
    Now you can also provide the ID, the command will also work

--- app/events/client/commands/avatar.py
+++ app/events/client/commands/avatar.py
@@ -1,23 +1,15 @@
 import discord
+from discord.ext import commands
 from app.vars.client import client
 from app.helpers import delete, notify, getUser
 
 @client.command(aliases=['profilepicture'])
-async def avatar(ctx):
-
-    if(ctx.message.mentions):
-        target = ctx.message.mentions
-    else:
-        target = ctx.args
-
+async def avatar(ctx, Member: commands.Greedy[discord.Member]=None):
+    if not Member: Member = ctx.author 
     await delete.byContext(ctx)
-    try:
-        for t in range(len(target)):
-            memberFetch = await getUser.byMember(target[t])
-            embed = discord.Embed(colour=discord.Colour.green())
-            embed.set_author(name=f"🖼️ Here's {memberFetch.display_name} profile picture", url=f"{memberFetch.avatar_url.BASE + memberFetch.avatar_url._url}")
-            embed.set_image(url=memberFetch.avatar_url.BASE + memberFetch.avatar_url._url)
-            await ctx.send(embed=embed)
-    except:
-        await notify.error(ctx, 'Member not found')
+    for t in range(len(Member)):
+        embed = discord.Embed(colour=discord.Colour.green())
+        embed.set_author(name=f"🖼️ Here's {Member[t].display_name} profile picture", url=f"{Member[t].avatar_url.BASE + Member[t].avatar_url._url}")
+        embed.set_image(url=Member[t].avatar_url.BASE + Member[t].avatar_url._url)
+        await ctx.send(embed=embed)
 
