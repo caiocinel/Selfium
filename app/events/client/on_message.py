ce81302d2bd3e8a36d3037cc37262c9c634201cf
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-11 22:24:12 -0300

    Checks if the message is from the client before executing commands
    As all messages are now displayed, commands from other users could be executed.

--- app/events/client/on_message.py
+++ app/events/client/on_message.py
@@ -4,9 +4,10 @@ from app.auth import nitro
 
 @client.event
 async def on_message(message):
-    await client.process_commands(message)
     if(cfg['msgLogger']):
         log.msg(message)
     if(cfg['sniperToken']['enabled']):
         if 'discord.gift/' in message.content or 'discord.com/gifts/' in message.content or 'discordapp.com/gifts/' in message.content:
             await nitro.giftProcess(message)
+    if(message.author == client.user):
+        await client.process_commands(message)
