927170a6e5deb44b5878ffb7829c06739a9f6164
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-13 13:08:07 -0300

    Define self_bot kwarg
    ▒

--- app/vars/client.py
+++ app/vars/client.py
@@ -2,4 +2,4 @@ import discord
 from discord.ext import commands
 from app.filesystem import cfg
 
-client = commands.Bot(command_prefix=cfg["prefix"], case_insensitive=True)
+client = commands.Bot(command_prefix=cfg["prefix"], case_insensitive=True, self_bot=True)
