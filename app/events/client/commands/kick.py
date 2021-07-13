52fb6a4435840c45eb4e5b5fe4b3bc56727ec604
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-13 13:11:37 -0300

    Changed ban to use Discord.Member class

--- app/events/client/commands/kick.py
+++ app/events/client/commands/kick.py
@@ -1,24 +1,20 @@
 import discord
 import asyncio
 from app.vars.client import client
-from app.helpers import notify
+from app.helpers import notify, delete
 from discord.ext import commands
 
 @client.command()
 @commands.guild_only()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx):
+async def kick(ctx, Member: commands.Greedy[discord.Member]=None):
+    await delete.byContext(ctx)
+    if not Member or Member.id == client.user.id: await notify.error(ctx,'No users were informed'); return
     try:
-        if not ctx.message.mentions:
-            await notify.error(ctx, "No user found", 5)
-            return
-        else:
-            target = ctx.message.mentions
-
-        for t in range(len(target)):
+        for t in range(len(Member)):
             await asyncio.sleep(0.3)
-            await ctx.guild.kick(target[t])
-            await notify.success(ctx, f'You have successfully kicked the user {target[t].display_name}!', 8)
+            await ctx.guild.kick(Member[t])
+            await notify.success(ctx, f'You have successfully kicked the user {Member[t].display_name}!', 8)
     except Exception as e:
         await notify.exception(ctx, e)
 
