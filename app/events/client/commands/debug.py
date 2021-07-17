961295b77fcbe25e1aaff46eadb5e97794e0d5b0
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-17 15:06:54 -0300

    Remove Test Function Parameters

--- app/events/client/commands/debug.py
+++ app/events/client/commands/debug.py
@@ -5,8 +5,7 @@ from app.helpers import delete, notify, getUser
 from app.filesystem import log
 
 @client.command()
-async def debug(ctx, msgid):
+async def debug(ctx):
     
 
     return
-    print(a)
