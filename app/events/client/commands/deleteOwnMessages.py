e95da2555b551d922f12e5ffe579b551865b17a2
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-17 14:26:12 -0300

    Recode DeleteOwnMessages using purge()
    Ty Copilot

--- app/events/client/commands/deleteOwnMessages.py
+++ app/events/client/commands/deleteOwnMessages.py
@@ -2,32 +2,23 @@ import asyncio
 from app.vars.client import client
 from app.helpers import notify, delete
 
+#Delete number of messages from DiscordPy client (This function was partially done by Github Copilot)
 @client.command(aliases=['removemymessages', 'dmm', 'clearmymessage'])
 async def deleteMyMessages(ctx, amount):
     await delete.byContext(ctx)
 
-    if(isinstance(int(float(amount)), int) and int(float(amount)) > 0):
-        messagesNumber = int(float(amount))
-    else:
-        await notify.error(ctx, 'Enter a valid lenght')
-        return
-        
-    if (messagesNumber > int(200)):
-        await notify.error(ctx, 'The current message limit is 200')
+    if int(amount) < 1:
+        await notify.error(ctx, 'Please enter a number greater than 0')
         return
 
-    if (messagesNumber):
-        try:
-            i = 0
-            async for message in ctx.message.channel.history().filter(lambda m: m.author == client.user):
-                if(i == messagesNumber):
-                    await notify.success(ctx, f' {messagesNumber} message(s) have been deleted!')
-                    return
-                try:
-                    await message.delete()
-                    i += 1
-                    await asyncio.sleep(0.33)
-                except:
-                    pass               
-        except:
-            await notify.exception(ctx)
+    try:
+        await ctx.message.channel.purge(limit=int(amount))
+        await notify.success(ctx, "Deleted {} messages from the chat".format(amount))
+    except:
+        await notify.error(ctx, "Something goes wrong, try again!")
+
+    
+
+
+
+
