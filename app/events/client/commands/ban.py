d62f3b3e94df3a251a0a113255f8d899d8c1b241
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-17 15:26:55 -0300

    Fix ban/kick

--- app/events/client/commands/ban.py
+++ app/events/client/commands/ban.py
@@ -9,9 +9,9 @@ from app.helpers import notify, delete
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, Member: commands.Greedy[discord.Member]=None):
     await delete.byContext(ctx)
-    if not Member or Member.id == client.user.id: await notify.error(ctx,'No users were informed'); return
     try:
         for t in range(len(Member)):
+            if not Member[t] or Member[t].id == client.user.id: await notify.error(ctx,'No users were informed');return            
             await asyncio.sleep(0.3)
             await ctx.guild.ban(Member[t])
             await notify.success(ctx, f'You have successfully banned the user {Member[t].display_name}!', 8)
--- app/events/client/commands/kick.py
+++ app/events/client/commands/kick.py
@@ -9,9 +9,9 @@ from discord.ext import commands
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, Member: commands.Greedy[discord.Member]=None):
     await delete.byContext(ctx)
-    if not Member or Member.id == client.user.id: await notify.error(ctx,'No users were informed'); return
     try:
         for t in range(len(Member)):
+            if not Member[t] or Member[t].id == client.user.id: await notify.error(ctx,'No users were informed');return            
             await asyncio.sleep(0.3)
             await ctx.guild.kick(Member[t])
             await notify.success(ctx, f'You have successfully kicked the user {Member[t].display_name}!', 8)
