fe3582092eb0d6c22e85aad93e498e47c1e890d0
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-17 14:58:15 -0300

    Delete voiceChannels command message

--- app/events/client/commands/voiceChannels.py
+++ app/events/client/commands/voiceChannels.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
-from app.helpers import notify
+from app.helpers import notify, delete
 from app.vars import client
 
 @commands.guild_only()
 @client.command(aliases=["voice_channels"])
 async def voiceChannels(ctx):
+    await delete.byContext(ctx)
     try:
         embed = discord.Embed(title="Voice Channels", colour=discord.Color.purple())
         embed.set_thumbnail(url=ctx.guild.icon_url)
