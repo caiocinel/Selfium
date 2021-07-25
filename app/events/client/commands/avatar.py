fdba19ce4b6ac9e999daf3b7b9d7a8c2696e037e
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-07-25 20:18:16 -0300

    Use embed handling in Avatar command

--- app/events/client/commands/avatar.py
+++ app/events/client/commands/avatar.py
@@ -1,15 +1,14 @@
 import discord
 from discord.ext import commands
 from app.vars.client import client
-from app.helpers import delete, notify, getUser
+from app.helpers import delete, sendEmbed
 
 @client.command(aliases=['profilepicture'])
-async def avatar(ctx, Member: commands.Greedy[discord.Member]=None):
-    if not Member: Member = ctx.author 
+async def avatar(ctx, Member: commands.Greedy[discord.Member]):
     await delete.byContext(ctx)
     for t in range(len(Member)):
         embed = discord.Embed(colour=discord.Colour.green())
         embed.set_author(name=f"🖼️ Here's {Member[t].display_name} profile picture", url=f"{Member[t].avatar_url.BASE + Member[t].avatar_url._url}")
         embed.set_image(url=Member[t].avatar_url.BASE + Member[t].avatar_url._url)
-        await ctx.send(embed=embed)
+        await sendEmbed(ctx,embed)
 
