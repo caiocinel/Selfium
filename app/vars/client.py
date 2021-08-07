7bbf9956ca60a189568120a04115b8a024f7ce25
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-07 20:51:01 -0300

    Disable Guild Subscription
    Fix slow on_ready event
    Not use in moment maybe later

--- app/vars/client.py
+++ app/vars/client.py
@@ -2,4 +2,4 @@ import discord
 from discord.ext import commands
 from app.filesystem import cfg
 
-client = commands.Bot(command_prefix=cfg["prefix"], case_insensitive=True, self_bot=True)
+client = commands.Bot(command_prefix=cfg["prefix"], case_insensitive=True, self_bot=True, guild_subscription_options = discord.GuildSubscriptionOptions.off())
