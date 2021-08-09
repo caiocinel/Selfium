def69654a6f56c308330b6022d33134a8c69e6d9
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-08 23:44:18 -0300

    Ban to Notify Class

--- app/events/client/commands/ban.py
+++ app/events/client/commands/ban.py
@@ -2,19 +2,22 @@ import discord
 import asyncio
 from discord.ext import commands
 from app.vars.client import client
-from app.helpers import notify, delete
+from app.helpers import Notify, delete
 
 @client.command()
 @commands.guild_only()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, Member: commands.Greedy[discord.Member]=None):
-    await delete.byContext(ctx)
+#@commands.has_permissions(ban_members=True)
+async def ban(ctx, Member: commands.Greedy[discord.Member] = None):
+    notify = Notify(ctx=ctx,name='Banning member...')
+    notify.prepair()            
+    if Member is None: 
+        notify.error(content='No users were informed')
+        return
     try:
-        for t in range(len(Member)):
-            if not Member[t] or Member[t].id == client.user.id: await notify.error(ctx,'No users were informed');return            
+        for t in range(len(Member)):       
             await asyncio.sleep(0.3)
             await ctx.guild.ban(Member[t])
-            await notify.success(ctx, f'You have successfully banned the user {Member[t].display_name}!', 8)
+            notify.success(content=f'You have successfully banned the user {Member[t].display_name}!')
     except Exception as e:
-        await notify.exception(ctx, e)
+        notify.exception(content=e)
 
