bd4ad4e2263627c21bc16cc49e51b995b497d112
ZeusHay <45320825+ZeusHay@users.noreply.github.com>
2021-08-13 14:43:42 -0300

    Added ConsoleLog and Allowlist to "KickAll" function

--- app/events/client/commands/kickAll.py
+++ app/events/client/commands/kickAll.py
@@ -1,13 +1,23 @@
 from discord.ext import commands
 from app.vars.client import client
 from app.helpers import notify
+from app.helpers.notify import ConsoleLog
 
 @client.command()
 @commands.guild_only()
 @commands.has_permissions(kick_members=True)
 async def kickAll(ctx):
+    AllowList = []
     for member in ctx.guild.members:
         if member.id != ctx.author.id:
-            await member.kick()
+            try:
+                if not(member.id in AllowList):
+                    await member.kick()
+                    ConsoleLog(f'[{member.display_name}]: Has been kicked.')
+                else:
+                    ConsoleLog(f'[{member.display_name}]: Has been skipped.')
+            except Exception as e:
+                ConsoleLog(f'[Error]: {e.text}')
+                pass
     else:
         await notify.success(ctx,f'All members successfully kicked')
