3305b726167b272a7f245a0a3d870bef18e4e162
ZeusHay <45320825+ZeusHay@users.noreply.github.com>
2021-08-14 07:51:48 -0300

    Added Log to Rename all and added "try" to member.edit

--- app/events/client/commands/renameAll.py
+++ app/events/client/commands/renameAll.py
@@ -3,12 +3,27 @@ import asyncio
 from discord.ext import commands
 from app.vars.client import client
 from app.helpers import notify
+from app.helpers.notify import ConsoleLog
 
 @client.command()
 @commands.guild_only()
 @commands.has_permissions(manage_nicknames=True)
 async def renameAll(ctx, *, nick: str):
     for member in ctx.guild.members:
-        await member.edit(nick=nick)
+        try:
+            await member.edit(
+                nick=nick
+            )
+
+            ConsoleLog(
+                f'[LOG]: {member.name} changed to {member.display_name}'
+            )
+
+        except Exception as e:
+            ConsoleLog(
+                f'[ERROR]: {e.text}'
+            )
     else:
-        await notify.success(ctx,f'All members have been successfully renamed to { nick }')
+        await notify.success(ctx,
+            f'All members have been successfully renamed to { nick }'
+        )
