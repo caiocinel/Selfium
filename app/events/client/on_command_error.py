5b6b531b99e0ec5fd871b671521d5b77f6498fa6
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-14 22:47:33 -0300

    Fix on_command_error

--- app/events/client/on_command_error.py
+++ app/events/client/on_command_error.py
@@ -4,8 +4,8 @@ from app.helpers import Notify, delete
 @client.event
 async def on_command_error(ctx, error):
     try:
-        notify = Notify(ctx=ctx, name="Exception")
-        notify.exception(content=error)
+        notify = Notify(ctx=ctx, title="Exception")
+        notify.exception(content=error.args[0])
     except:
         pass
 
