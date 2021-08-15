941df3edf0c1d0cafb46a079ef7f5314533c0c18
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-15 00:56:33 -0300

    Channels to Notify Class

--- app/events/client/commands/channels.py
+++ app/events/client/commands/channels.py
@@ -1,5 +1,5 @@
 import discord
-from app.helpers import getUser, notify, delete
+from app.helpers import getUser, Notify
 from app.vars.client import client
 from discord.ext import commands
 
@@ -7,21 +7,16 @@ from discord.ext import commands
 @commands.guild_only()
 @client.command(aliases=["serverChannels"])
 async def channels(ctx):
-    try:
-        embed = discord.Embed(title="Server Channels", colour=discord.Color.purple())
-        embed.set_thumbnail(url=ctx.guild.icon_url)
-        embed.add_field(name="Voice Channels", value="** **", inline=False)
-        for channel in ctx.guild.voice_channels:
-            embed.add_field(name="\u2800", value=f"```{channel.name}```")
-        embed.add_field(name="Categories", value="** **", inline=False)
-        for channel in ctx.guild.categories:
-            embed.add_field(name="\u2800", value=f"```{channel.name}```")
-        embed.add_field(name="Text Channels", value="** **", inline=False)
-        for channel in ctx.guild.text_channels:
-            embed.add_field(name="\u2800", value=f"```{channel.name}```")
-        embed.set_footer(text="Selfium (◔‿◔)")  
-        await ctx.send(embed=embed)
-    except Exception as e:
-        print(e)
-        await notify.error(ctx, "Something goes wrong, check console for logs")
+    notify = Notify(ctx=ctx, title="Server Channels", color=discord.Color.purple())
+    fields = [("Voice Channels", "** **", False)]
+    for channel in ctx.guild.voice_channels:
+        fields.append(("\u2800", f"```{channel.name}```", True))
+    fields.append(("Categories", "** **", False))
+    for channel in ctx.guild.categories:
+        fields.append(("\u2800", f"```{channel.name}```", True))
+    fields.append(("Text Channels", "** **", False))
+    for channel in ctx.guild.text_channels:
+        fields.append(("\u2800", f"```{channel.name}```", True))
+    notify.fields(fields=fields)
+
 
