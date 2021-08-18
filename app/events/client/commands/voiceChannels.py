9a5ee0972e80b4a1f216fce8d29d8811612fe117
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-17 23:27:30 -0300

    voiceChannels to Notify Class

--- app/events/client/commands/voiceChannels.py
+++ app/events/client/commands/voiceChannels.py
@@ -1,19 +1,13 @@
 import discord
 from discord.ext import commands
-from app.helpers import notify, delete
+from app.helpers import Notify
 from app.vars import client
 
 @commands.guild_only()
 @client.command(aliases=["voice_channels"])
 async def voiceChannels(ctx):
-    await ctx.message.delete()
-    try:
-        embed = discord.Embed(title="Voice Channels", colour=discord.Color.purple())
-        embed.set_thumbnail(url=ctx.guild.icon_url)
-        for channel in ctx.guild.voice_channels:
-            embed.add_field(name="\u2800", value=f"```{channel.name}```")
-        embed.set_footer(text="Selfium (◔‿◔)")  
-        await ctx.send(embed=embed)
-    except Exception as e:
-        print(e)
-        await notify.error(ctx, "Something goes wrong, check console for logs")
+    notify = Notify(ctx=ctx, title='Server Voice Channels...')
+    fields = []
+    for channel in ctx.guild.voice_channels:
+        fields.append(("\u2800", f"```{channel.name}```", True))
+    notify.fields(fields=fields)
