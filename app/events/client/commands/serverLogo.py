f6c5f6e120ad94fff636b030b39889a877345350
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-08-17 23:03:44 -0300

    serverLogo to Notify Class

--- app/events/client/commands/serverLogo.py
+++ app/events/client/commands/serverLogo.py
@@ -1,15 +1,13 @@
 import discord
 from discord.ext import commands
 from app.vars.client import client
-from app.helpers import delete, notify, getUser
+from app.helpers import Notify
 
 @commands.guild_only()
 @client.command(aliases=['servericon','guildicon','guildlogo'])
 async def serverLogo(ctx):
-    await ctx.message.delete()
-    embed = discord.Embed(colour=discord.Colour.green())
-    embed.set_author(name=f"🖼️ Here's {ctx.guild.name} logo", url=f"{ctx.guild.icon_url.BASE + ctx.guild.icon_url._url}")
-    embed.set_image(url=ctx.guild.icon_url.BASE + ctx.guild.icon_url._url)
-    await ctx.send(embed=embed)
+    notify = Notify(ctx=ctx, title='Server Icon')
+    notify.prepair()
+    notify.image(title=f"🖼️ Here's {ctx.guild.name} logo", image=f"{ctx.guild.icon_url.BASE + ctx.guild.icon_url._url}")
 
 
