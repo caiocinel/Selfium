4dc4a331c5b0d396285782a3332dc905a096b454
Caillou <60858720+caiocinel@users.noreply.github.com>
2021-10-11 22:55:27 -0300

    Revert "Add deleteAllWebhooks command"
    This reverts commit 3c50d556f281cf82f12bfa3c3548f96e1a3527b5.

--- app/events/client/commands/__init__.py
+++ app/events/client/commands/__init__.py
@@ -21,7 +21,6 @@ from .kickAll import *
 from .removeAllFriends import *
 from .deleteOwnMessages import *
 from .deleteAllRoles import *
-from .deleteAllWebhooks import *
 from .changeToken import *
 from .embed import *
 from .ip import *
